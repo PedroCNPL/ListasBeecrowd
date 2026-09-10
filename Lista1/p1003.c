cc2bce9832505faad7b6166afec5777a2e110f96
PedroCNPL <pedrolagepl7@gmail.com>
2026-09-10 14:51:09 -0300

    problema 1003

---
+++ Lista1/p1003.c
@@ -0,0 +1,14 @@
+    #include <stdio.h>
+    
+    int main() {
+
+        int a, b, X;
+
+        scanf("%d", &a);
+        scanf("%d", &b);
+
+        X = a + b;
+
+        printf("SOMA = %d\n", X);
+        return 0;
+    }
